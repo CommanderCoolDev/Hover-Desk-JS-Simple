d7e4067cd5a87aa9bf7dc67e4e70138cb19edd8c
Dima <78321515+CommanderCoolDev@users.noreply.github.com>
2021-09-18 22:59:42 +0300

    some fix

--- index.js
+++ index.js
@@ -7,22 +7,23 @@
     const square = document.createElement('div');
 
     square.classList.add('square');
-    square.addEventListener('mouseover', () => setColor(square));
-    square.addEventListener('mouseleave', () => removeColor(square));
+    square.addEventListener('mouseover', setColor);
+    square.addEventListener('mouseleave', removeColor);
     board.append(square);
  }
 
- function setColor (el) {
+ function setColor (ev) {
+    const el = ev.target;
      const color = getRandomColor();
      el.style.backgroundColor = color;
      el.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
  }
- function removeColor(el) {
+ function removeColor(ev) {
+   const el = ev.target;
     el.style.backgroundColor = '#1d1d1d';
     el.style.boxShadow = `0 0 2px #000`;
 
  }
  function getRandomColor() {
-const index = Math.floor(Math.random() * colors.length);
-return colors[index];
+return colors[Math.floor(Math.random() * colors.length)];
  }
